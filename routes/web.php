5f6ba4f3c23de34a76d6364eb842148f2cb8f825
chunzz <chiennvph06047@fpt.edu.vn>
2019-07-18 20:12:31 +0700

    up code

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', function () {
 		return view('welcome');	
 	});
+Route::get('students', 'StudentController@index')->name('students');	
 Route::get('users/{id}/{name}', function ($id,$name) {
 		return 'Tham số là:'.$id.'---'.$name;	
 	});
